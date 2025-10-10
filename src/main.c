013f036b87f3d4754ad2ae7e7b817673d1dd9b38
Gerald <126714838+GeraldlTzy@users.noreply.github.com>
2025-10-10 16:57:13 -0600

    Actualizar main.c

--- src/main.c
+++ src/main.c
@@ -29,6 +29,33 @@ void canonize(int** mat, int rows, int cols, int pivot_row, int pivot_col){
         }
     }
 }
+
+void maximize(int** mat, int rows, int cols){
+    int min = MAX_VAL;
+    int pivot_row = -1, pivot_col = -1;;
+
+    for(int c = 1; c < cols-1; ++c){
+        if(min > mat[0][c]){
+            min = mat[0][c];
+            pivot_col = c;
+        }
+    }
+    if (min >= 0)
+        return;
+
+    for(int r = 1; r < rows; ++r){
+        if(mat[r][pivot_col] > 0 && min > mat[r][pivot_col]){
+
+            min = mat[r][pivot_col];
+            pivot_row = r;
+        }
+    }
+    if(pivot_row < 0)
+        return;
+    canonize(mat, rows, cols, pivot_col, pivot_row);
+
+}
+
 int main(int argc, char* args[]){
     printf("Hello World!\n");
     
